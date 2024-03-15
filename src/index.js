931826c9c35ae3ec89d9a8ac8cee2e0676d1e048
jayeshmali1234 <130337734+jayeshmali1234@users.noreply.github.com>
2024-03-15 09:43:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import DashBoard from './DashBoards';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
     <BrowserRouter>
     <DashBoard/>
     </BrowserRouter>
-  // {/* </React.StrictMode> */}
 );
-
-
-// reportWebVitals();
